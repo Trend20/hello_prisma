e1cd4a85f7fdea12db502f7f9ddaced47157ffd6
Trend20 <enockomondi305@gmail.com>
2024-03-05 11:48:42 +0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.post.update({
+    where: {
+      slug: "my-first-post",
+    },
+    data: {
+      comments: {
+        createMany: {
+          data: [
+            { comment: "Great post!" },
+            { comment: "Can't wait to read more!" },
+          ],
+        },
+      },
+    },
+  });
+
+  await prisma.user.create({
+    data: {
+      name: "Rich",
+      email: "hello@prisma.com",
+      posts: {
+        create: {
+          title: "My first post",
+          body: "Lots of really interesting stuff",
+          slug: "my-first-post",
+        },
+      },
+    },
+  });
+  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
+
+  const posts = await prisma.post.findMany({
+    include: {
+      comments: true,
+    },
+  });
+
+  console.dir(posts, { depth: Infinity });
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
